6ced660c53edf8acb380c17b34f1c5dfacc44681
thomdavin <thomas.davin@ymail.com>
2015-03-05 14:32:20 +0100

    classe encapsulation

---
+++ src/main/java/outils/Encapsulation.java
@@ -0,0 +1,20 @@
+package outils;
+
+import java.util.List;
+
+public class Encapsulation {
+	
+	public List<String> liste;  
+	
+	
+	public Encapsulation(){}
+	
+	
+	public Encapsulation(List<String> nouvListe){
+		this.liste=nouvListe;
+	}
+	
+	
+	
+	
+}
